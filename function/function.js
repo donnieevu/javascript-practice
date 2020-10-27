addf7c7309caac8917a6f0196cbc88cc0deccfe2
Donald Vu <donaldhdvu@gmail.com>
2020-10-27 17:40:11 -0500

    function practice, week

--- function/function.js
+++ function/function.js
@@ -1,33 +1,52 @@
 // 2 Parts of functions
-function singSong() {
-    console.log("DO");
-    console.log("RE");
-    console.log("MI");
-} //part 1 
-singSong(); //part 2
+// function singSong() {
+//     console.log("DO");
+//     console.log("RE");
+//     console.log("MI");
+// } //part 1 
+// singSong(); //part 2
 
 //FUNCTION WITH PARAMETER(S)
 
-function greet(firstName, lastName) {
-    console.log(`Hello there, ${firstName} ${lastName[0]}.`);
-}
-greet("Naruto", "Uzamaki")
+// function greet(firstName, lastName) {
+//     console.log(`Hello there, ${firstName} ${lastName[0]}.`);
+// }
+// greet("Naruto", "Uzamaki")
 
-function repeat(msg, numTimes) {
-    let result = ''
-    for (let i = 0; i < numTimes; i++) {
-        result += msg;
-    }
-    console.log(result);
-}
-repeat("yo yo wassup ", 3);
+// function repeat(msg, numTimes) {
+//     let result = ''
+//     for (let i = 0; i < numTimes; i++) {
+//         result += msg;
+//     }
+//     console.log(result);
+// }
+// repeat("yo yo wassup ", 3);
 
-//return keyword practice
-function add(x, y) {
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        return false;
+//RETURN KEYWORD PRACTICE
+// function add(x, y) {
+//     if (typeof x !== 'number' || typeof y !== 'number') {
+//         return false;
+//     }
+//     return x + y;
+// }
+// console.log(add(1, 2));
+// console.log(add(4, 'a'));
+
+//FUNCTION PRACTICE
+function returnDay(day) {
+    let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    if (day < 1 || day > 7) {
+        return null;
     }
-    return x + y;
+    for (let i = 1; i <= week.length; i++) {
+        if (i === day) {
+            return week[i - 1];
+        }
+    }
 }
-console.log(add(1, 2));
-console.log(add(4, 'a')); 
+
+console.log(returnDay(1));
+console.log(returnDay(7));
+console.log(returnDay(4));
+console.log(returnDay(0));
+
