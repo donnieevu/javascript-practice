68d7e8a4211101ab12be381ac068d32bab2fd822
Donald Vu <donaldhdvu@gmail.com>
2020-10-27 02:47:27 -0500

    to do list update

--- toDoListProject/toDoList.js
+++ toDoListProject/toDoList.js
@@ -5,16 +5,23 @@ while (input !== 'quit' && input !== 'q') {
 
     if (input === 'list') {
         console.log('***********');
+        console.log('TODO LIST');
         for (let i = 0; i < todos.length; i++) {
             console.log(`${i}: ${todos[i]}`);
         }
         console.log('***********');
     } else if (input === 'new') {
-        input = prompt('add a todo');
-        todos.push(input);
+        const newTodo = prompt('add a todo');
+        todos.push(newTodo);
+        console.log(`${newTodo} was added to list`);
     } else if (input === 'delete') {
-        input = prompt("enter index you want to delete");
-        todos.splice(input, 1);
+        const index = parseInt(prompt("enter index you want to delete"));
+        if (!Number.isNaN(index) && index <= todos.length) {
+            const deleted = todos.splice(index, 1);
+            console.log(`to do item "${deleted[0]}" was deleted.`);
+        } else {
+            console.log("Unknown index, select index within list.");
+        }
     }
     input = prompt('What would you like to do?');
 }
