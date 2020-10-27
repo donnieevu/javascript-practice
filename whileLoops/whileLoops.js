49439ba8b21ece668d9e06afcd1b6aa6145fe983
Donald Vu <donaldhdvu@gmail.com>
2020-10-26 21:53:58 -0500

    break keyword practice

--- whileLoops/whileLoops.js
+++ whileLoops/whileLoops.js
@@ -1,6 +1,21 @@
-let count = 1;
+// let count = 1;
+// while (count <= 10) {
+//     console.log(count);
+//     count++;
+// }
 
-while (count <= 10) {
-    console.log(count);
-    count++;
+// const secret = 'hipstur';
+
+// let guess = prompt('enter secret code...');
+
+// while (guess !== secret) {
+//     guess = prompt('enter secret code....')
+// }
+// console.log("WELCOME TO THE SECRET SOCIETY!!");
+
+let input = prompt("say something")
+while (true) {
+    input = prompt(input)
+    if (input.toLowerCase() === "stop") break;
 }
+console.log("OK ill stop");
