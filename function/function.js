3feb30d263d414ed470394e90dd795b116a1ca41
Donald Vu <donaldhdvu@gmail.com>
2020-10-27 15:20:31 -0500

    functions practice

---
+++ function/function.js
@@ -0,0 +1,23 @@
+// 2 Parts of functions
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+} //part 1 
+singSong(); //part 2
+
+//FUNCTION WITH PARAMETER(S)
+
+function greet(firstName, lastName) {
+    console.log(`Hello there, ${firstName} ${lastName[0]}.`);
+}
+greet("Naruto", "Uzamaki")
+
+function repeat(msg, numTimes) {
+    let result = ''
+    for (let i = 0; i < numTimes; i++) {
+        result += msg;
+    }
+    console.log(result);
+}
+repeat("yo yo wassup ", 3);
