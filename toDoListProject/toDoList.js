68b841f0d471610174d627757d1b7e3555301fd1
Donald Vu <donaldhdvu@gmail.com>
2020-10-27 02:48:24 -0500

    formatting

--- toDoListProject/toDoList.js
+++ toDoListProject/toDoList.js
@@ -10,16 +10,21 @@ while (input !== 'quit' && input !== 'q') {
             console.log(`${i}: ${todos[i]}`);
         }
         console.log('***********');
-    } else if (input === 'new') {
+    }
+
+    else if (input === 'new') {
         const newTodo = prompt('add a todo');
         todos.push(newTodo);
         console.log(`${newTodo} was added to list`);
-    } else if (input === 'delete') {
+    }
+
+    else if (input === 'delete') {
         const index = parseInt(prompt("enter index you want to delete"));
         if (!Number.isNaN(index) && index <= todos.length) {
             const deleted = todos.splice(index, 1);
             console.log(`to do item "${deleted[0]}" was deleted.`);
-        } else {
+        }
+        else {
             console.log("Unknown index, select index within list.");
         }
     }
