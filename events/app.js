ac7a5373eeb0341d53bc945046e1340e37762d00
Donald Vu <donaldhdvu@gmail.com>
2020-11-09 02:03:32 -0600

    addEventListener Practice

--- events/app.js
+++ events/app.js
@@ -13,3 +13,20 @@ btn.onmouseenter = scream;
 
 const h1 = document.querySelector('h1')
 h1.onclick = () => (alert('you clicked h1'))
+
+const btn3 = document.querySelector('#v3')
+btn3.addEventListener('dblclick', () => {
+    alert("CLICKED!");
+})
+
+function twist() {
+    console.log("twist");
+}
+
+function shout() {
+    console.log("shout");
+}
+
+const tasButton = document.querySelector("#tas")
+tasButton.addEventListener('click', twist)
+tasButton.addEventListener('click', shout)
