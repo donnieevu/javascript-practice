ddf41008d7c5317c4cc4ed96a89f5074a697ef20
Donald Vu <donaldhdvu@gmail.com>
2020-11-09 02:04:05 -0600

    added comment

--- events/app.js
+++ events/app.js
@@ -10,10 +10,10 @@ function scream() {
 }
 btn.onmouseenter = scream;
 
-
 const h1 = document.querySelector('h1')
 h1.onclick = () => (alert('you clicked h1'))
 
+// ADD EVENT LISTENER PRACTICE
 const btn3 = document.querySelector('#v3')
 btn3.addEventListener('dblclick', () => {
     alert("CLICKED!");
