c83a9c6272efafd5baa80a434bf662a03d95e23f
Donald Vu <donaldhdvu@gmail.com>
2020-11-10 02:49:47 -0600

    form event practice, grocery list

--- events/formEvents/app.js
+++ events/formEvents/app.js
@@ -1,24 +1,43 @@
-const tweetForm = document.querySelector('#tweetForm');
-const tweetContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
+// const tweetForm = document.querySelector('#tweetForm');
+// const tweetContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener('submit', function (e) {
 
+//     e.preventDefault();
+//     // const usernameInput = document.querySelectorAll('input')[0];
+//     // const tweetInput = document.querySelectorAll('input')[1];
+//     // console.log(`${tweetForm.elements.username.value}: ${this.elements.tweet.value}`); //2 diff ways of using it (this)
+//     const usernameInput = tweetForm.elements.username;
+//     const tweetInput = this.elements.tweet
+//     addTweet(usernameInput.value, tweetInput.value)
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+// })
+
+// const addTweet = (username, tweet) => {
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+
+//     bTag.append(username);
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`);
+//     tweetContainer.append(newTweet);
+// }
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
-    // console.log(`${tweetForm.elements.username.value}: ${this.elements.tweet.value}`); //2 diff ways of using it (this)
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = this.elements.tweet
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
+    const qtyInput = this.elements.qty;
+    const productInput = this.elements.product;
+
+    addItem(qtyInput.value, productInput.value)
+    qtyInput.value = '';
+    productInput.value = '';
 })
 
-const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-
-    bTag.append(username);
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`);
-    tweetContainer.append(newTweet);
+const addItem = (qty, product) => {
+    const newItem = document.createElement('li')
+    const list = document.querySelector("#list")
+    newItem.append(`${qty} ${product}s `);
+    list.append(newItem)
 }
