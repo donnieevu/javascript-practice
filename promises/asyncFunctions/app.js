4ef37529d33fb0d3d438d9b0b90eaaf54eaf3b20
Donald Vu <donaldhdvu@gmail.com>
2020-11-17 02:52:27 -0600

    await keyword & try/catch

--- promises/asyncFunctions/app.js
+++ promises/asyncFunctions/app.js
@@ -26,5 +26,71 @@ login('aznfreak8178')
         console.log(msg);
     })
     .catch((err) => {
-        console.log("ERROR", err);
+        console.log("ERROR:", err);
     })
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('indigo', 1000))
+//     .then(() => delayedColorChange('violet', 1000))
+
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000) // this line will only run until after the red is resolved. 
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+    return 'ALL DONE'
+}
+
+// rainbow().then(() => {
+//     console.log("END OF RAINBOW!");
+// })
+
+async function printRainbow() {
+    await rainbow();
+    console.log("END OF RAINBOW!");
+}
+printRainbow();
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+
+        setTimeout(() => {
+            if (rand < .7) {
+                resolve(`Here is your fake data from ${url}`)
+            }
+            reject('Connection Timeout :/!')
+        }, 1000)
+    })
+}
+
+async function makeTwoRequest() {
+    try {
+        let data1 = await fakeRequest('/page1')
+        let data2 = await fakeRequest('/page2')
+        console.log(data1);
+        console.log(data2);
+    } catch (e) {
+        console.log("CAUGHT AN ERROR");
+        console.log('error is: ', e);
+    }
+
+}
